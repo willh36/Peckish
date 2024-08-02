1ee06fb2712bb9ac17da76b341733354e9bb68a0
willh36 <92693911+willh36@users.noreply.github.com>
2024-08-02 17:26:12 +0100

    Initial Commit

---
+++ Peckish/PeckishApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeckishApp.swift
+//  Peckish
+//
+//  Created by Will Hayes on 02/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct PeckishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
